2058eea0194f7c498a6c34e4d3fb672bea994bd1
Nisha Varu <nishasapariya242@gmail.com>
2024-01-16 16:31:50 -0600

    portfolio

--- js/main.js
+++ js/main.js
@@ -97,3 +97,60 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+function fade() {
+  $('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 8000);
+
+
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+
+
+// const customCursor = document.getElementById('custom-cursor');
+// const hoverContainer = document.querySelector('.main-container');
+// const updateCursorPosition = (event) => {
+//   customCursor.style.top = `${event.clientY}px`;
+//   customCursor.style.left = `${event.clientX}px`;
+// }
+// window.addEventListener('mousemove', (event) => {
+//   updateCursorPosition(event)
+  
+//   if (hoverContainer.matches(':hover')) {
+//     customCursor.classList.add('zoom')
+//   } else {
+//     customCursor.classList.remove('zoom')
+//   }
+// })
+
+const cursor = document.querySelector(".custom-cursor")
+document.addEventListener('mousemove', (e) => {
+  // const customCursor = document.querySelector('.custom-cursor');
+  // customCursor.style.top = e.clientY + 'px';
+  // customCursor.style.left = e.clientX + 'px';
+
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+
+});
